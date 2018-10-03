76e54ce42b1a6d8a35dd6435f5a6c2a7852e34b6
Filipe Oliveira <filipeoliveirah@gmail.com>
2018-10-03 16:31:31 -0300

    Upload

--- head.php
+++ head.php
@@ -9,11 +9,13 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="<?php //bloginfo('template_url');?>node_modules/bootstrap/compiler/bootstrap.css">
-    <link rel="stylesheet" media="(max-width: 1199px)" href="<?php //bloginfo('template_url');?>node_modules/bootstrap/compiler/style.css" />
+    <link rel="stylesheet" media="(max-width: 1199px)" href="<?php //bloginfo('template_url');?>node_modules/bootstrap/compiler/style-mobile.css" />
     <link rel="stylesheet" media="(min-width: 1200px)" href="<?php //bloginfo('template_url');?>node_modules/bootstrap/compiler/style.css" />
-    <link rel="stylesheet" href="<?php //bloginfo('template_url');?>node_modules/animate.css">    
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,700,900" rel="stylesheet">
+    
+    <link rel="stylesheet" href="<?php //bloginfo('template_url');?>node_modules/animate.css">
 
+    <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,700,900" rel="stylesheet">
 
     <!-- Favicons -->
     <link rel="apple-touch-icon" href="https://getbootstrap.com/docs/4.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
--- nossos-premios.php
+++ nossos-premios.php
@@ -1,24 +1,23 @@
 <div class="container-fluid nossos-premios">
     <div class="container my-container">
-    <div class="cabecalho-light">
-        <h2>Nossos Prêmios</h2>  
-        <span></span> 
-    </div>
-    <div class="row">
-        <div class="col-md-3">        
-        <img class="d-block w-100 rounded" src="inc/img/premio-gran-awards-2012.jpg" alt="Second slide">  
+        <div class="cabecalho-light">
+            <h2>Nossos Prêmios</h2>  
+            <span></span> 
         </div>
-        <div class="col-md-3">        
-        <img class="d-block w-100 rounded" src="inc/img/premio-iso9001.jpg" alt="Second slide">  
+        <div class="row">
+            <div class="col-md-3 col-6">        
+                <img class="d-block w-100 rounded" src="inc/img/premio-gran-awards-2012.jpg" alt="Second slide">  
+            </div>
+            <div class="col-md-3 col-6">        
+                <img class="d-block w-100 rounded" src="inc/img/premio-iso9001.jpg" alt="Second slide">  
+            </div>
+            <div class="col-md-3 col-6">        
+                <img class="d-block w-100 rounded" src="inc/img/premio-valence.jpg" alt="Second slide">  
+            </div>
+            <div class="col-md-3 col-6">        
+                <img class="d-block w-100 rounded" src="inc/img/logo-MBI.jpg" alt="Second slide">  
+            </div>
         </div>
-        <div class="col-md-3">        
-        <img class="d-block w-100 rounded" src="inc/img/premio-valence.jpg" alt="Second slide">  
-        </div>
-        <div class="col-md-3">        
-        <img class="d-block w-100 rounded" src="inc/img/logo-MBI.jpg" alt="Second slide">  
-        </div>
-    </div>
-    <a href="sobre.php"><button class="btn botao-laranja" type="button">Veja todos</button></a>
-
+        <a href="sobre.php"><button class="btn botao-laranja" type="button">Veja todos</button></a>
     </div>
 </div>
