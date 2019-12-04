f6c7e401e004af8c02bde32fa061ecb02d34f9a3
Vinicius de Santana <santana@dnadevendas.com.br>
2019-12-04 16:49:47 -0300

    change align to menu

--- wp-content/themes/lafaete/page-produtos.php
+++ wp-content/themes/lafaete/page-produtos.php
@@ -9,13 +9,13 @@
 	<div class="blog-floater">
         <div class="container">
             <div class="row"> 
-                <div class="col-md-4">
+                <div class="col-md-3">
                   <svg class="svg-inline--fa fa-home fa-w-18" aria-hidden="true" data-icon="home" data-prefix="fas" role="img" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
                   <path d="M488 312.7V456c0 13.3-10.7 24-24 24H348c-6.6 0-12-5.4-12-12V356c0-6.6-5.4-12-12-12h-72c-6.6 0-12 5.4-12 12v112c0 6.6-5.4 12-12 12H112c-13.3 0-24-10.7-24-24V312.7c0-3.6 1.6-7 4.4-9.3l188-154.8c4.4-3.6 10.8-3.6 15.3 0l188 154.8c2.7 2.3 4.3 5.7 4.3 9.3zm83.6-60.9L488 182.9V44.4c0-6.6-5.4-12-12-12h-56c-6.6 0-12 5.4-12 12V117l-89.5-73.7c-17.7-14.6-43.3-14.6-61 0L4.4 251.8c-5.1 4.2-5.8 11.8-1.6 16.9l25.5 31c4.2 5.1 11.8 5.8 16.9 1.6l235.2-193.7c4.4-3.6 10.8-3.6 15.3 0l235.2 193.7c5.1 4.2 12.7 3.5 16.9-1.6l25.5-31c4.2-5.2 3.4-12.7-1.7-16.9z"/>
                   </svg>
                   <?php wp_custom_breadcrumbs() ?>
                 </div>
-                <div class="col-md-4 formulario">
+                <div class="col-md-6 formulario">
 					<?php $search = $_GET['searchkey'];?>
 					<form ROLE="search" action="<?php echo($_SERVER['REQUEST_URI']); ?>" method="get">
 						<div>
@@ -25,7 +25,7 @@
 						</div>
 					</form>    
                 </div>
-                <div class="col-md-4 d-sm-flex">
+                <div class="col-md-3 d-sm-flex">
                   <div class="blog-categorias">
                       <svg enable-background="new 0 0 24 24" version="1.1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">                        
                         <path d="M24,3c0-0.6-0.4-1-1-1H1C0.4,2,0,2.4,0,3v2c0,0.6,0.4,1,1,1h22c0.6,0,1-0.4,1-1V3z"/>
@@ -59,26 +59,7 @@
         </div>    
       </div>      
     </div>
-    <div id="produtos">
-      <div class="container produto-floater">
-        <div class="row">
-          <div class="col-md-8 texto">
-            <div class="bread">
-              <svg class="svg-inline--fa fa-home fa-w-18" aria-hidden="true" data-icon="home" data-prefix="fas" role="img" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
-              <path d="M488 312.7V456c0 13.3-10.7 24-24 24H348c-6.6 0-12-5.4-12-12V356c0-6.6-5.4-12-12-12h-72c-6.6 0-12 5.4-12 12v112c0 6.6-5.4 12-12 12H112c-13.3 0-24-10.7-24-24V312.7c0-3.6 1.6-7 4.4-9.3l188-154.8c4.4-3.6 10.8-3.6 15.3 0l188 154.8c2.7 2.3 4.3 5.7 4.3 9.3zm83.6-60.9L488 182.9V44.4c0-6.6-5.4-12-12-12h-56c-6.6 0-12 5.4-12 12V117l-89.5-73.7c-17.7-14.6-43.3-14.6-61 0L4.4 251.8c-5.1 4.2-5.8 11.8-1.6 16.9l25.5 31c4.2 5.1 11.8 5.8 16.9 1.6l235.2-193.7c4.4-3.6 10.8-3.6 15.3 0l235.2 193.7c5.1 4.2 12.7 3.5 16.9-1.6l25.5-31c4.2-5.2 3.4-12.7-1.7-16.9z"/>
-              </svg>
-              <p> Home » Produtos </p>
-            </div>
-            <p><?php if(have_posts()): the_post(); the_content(); endif; ?></p>
-            <a href="#faca-um-orcamento"><button class="btn">Faça um orçamento agora</button></a>
-          </div>
-          <div class="col-md-4 pagamento">
-            <h4>Condições de Pagamento</h4>
-            <img src="<?php bloginfo('template_url');?>/inc/img/pagseguro.png">
-          </div>
-        </div>
-      </div>
-    </div>
+    <?php include_once('inc/produtos-orcamento-agora.php'); ?>
     <div class="container produtos-container">
       <div class="row">
 		<?php
@@ -143,22 +124,24 @@
         ?>
 
         <div class="default-service-column col-md-4">
-          <div class="inner-box">
-              <div class="inner-most">
-                <figure class="image-box">
-                  <img width="100%" height="270" src="<?php echo $image['url']; ?>" class="img-responsive wp-post-image" alt="<?php echo $image['alt']; ?>">
-                </figure>
-                <div class="lower-part">
-                    <div class="left-curve"></div>
-                    <div class="right-curve"></div>                    
-                    <div class="content">
-                      <h3><?php echo $term->name; ?></h3>
-                      <p><?php echo $term->description;?></p>
-                      <div class="more-link"><a href="<?php bloginfo('url')?>/produtos/<?php echo $term->slug; ?>" class="read-more">Clique aqui</a></div>
-                    </div>
-                </div>
-              </div>
-          </div>
+        	<a href="<?php bloginfo('url')?>/produtos/<?php echo $term->slug; ?>" class="card-text">
+				<div class="inner-box">
+				  <div class="inner-most">
+				    <figure class="image-box">
+				      <img width="100%" height="270" src="<?php echo $image['url']; ?>" class="img-responsive wp-post-image" alt="<?php echo $image['alt']; ?>">
+				    </figure>
+				    <div class="lower-part">
+				        <div class="left-curve"></div>
+				        <div class="right-curve"></div>                    
+				        <div class="content">
+				          <h3><?php echo $term->name; ?></h3>
+				          <p><?php echo $term->description;?></p>
+				          <div class="more-link"><a href="<?php bloginfo('url')?>/produtos/<?php echo $term->slug; ?>" class="read-more">Clique aqui</a></div>
+				        </div>
+				    </div>
+				  </div>
+				</div>
+      		</a>
         </div>
 		<?php
 			endforeach; endif;
@@ -170,8 +153,15 @@
 				<div class="line-L col-6">
 					<?php
 					//links da paginação
-					$prev = get_prev_page_link( $produtos->max_num_pages);
-					$next = get_next_page_link( $produtos->max_num_pages);
+					if( isset( $search ) ){
+						$prev = get_prev_page_link( $produtos->max_num_pages);
+						$next = get_next_page_link( $produtos->max_num_pages);
+					}else{
+						$termsSize = count($terms);
+						$max_num_pages = $termsSize/$postsPerPage;
+						$prev = get_prev_page_link( $max_num_pages);
+						$next = get_next_page_link( $max_num_pages);
+					}
 						if($prev){
 							echo "<a class='page-btn' href='".$prev."'>";
 							echo "Anterior";
