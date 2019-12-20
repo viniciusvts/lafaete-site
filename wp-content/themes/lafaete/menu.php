4a63f127eb9ca1f6e6a62fec82f88f562a6ea4ff
Vinicius de Santana <santana@dnadevendas.com.br>
2019-12-20 11:32:54 -0300

    change link from blog to artigos

--- wp-content/themes/lafaete/menu.php
+++ wp-content/themes/lafaete/menu.php
@@ -116,8 +116,7 @@
           Conteúdo
         </a>
         <div class="dropdown-menu txtAlignCenter" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="<?php bloginfo('url'); ?>/blog">Blog</a>
-          <a class="dropdown-item" href="<?php bloginfo('url'); ?>/materiais">Materiais</a>
+          <a class="dropdown-item" href="<?php bloginfo('url'); ?>/artigos/">Blog</a>
         </div>
       </li>
 
