f91b89f9d5b322ea17a0e6598ad645085fe40888
Vinicius de Santana <santana@dnadevendas.com.br>
2019-12-20 14:14:49 -0300

    fix styles in mateiriais

--- wp-content/themes/lafaete/page-materiais.php
+++ wp-content/themes/lafaete/page-materiais.php
@@ -1,42 +1,62 @@
 <?php /* Template Name: Blog */ ?>
-
 <!doctype html>
 <html lang="pt-br">
   <?php include_once('head.php'); ?>
-
   <body>
     <?php include_once('menu.php'); ?>
-
     <?php include_once('flat-header.php'); ?>
 
-    <div class="container blog">
-        <div class="row">
-          <?php
-          $postsPerPage = get_option( 'posts_per_page' );
-          $paged = isset( $_GET['sheet'] )? $_GET['sheet'] : 1;
-          $args = array(
-            'posts_per_page' => $postsPerPage,
-            'paged' => $paged,
-            'post_type' => 'materiais',
-          );
-          $card = new WP_Query($args);
-          while($card->have_posts()) : $card->the_post();
-          $link = get_field('link'); ?>
-          <div class="col-md-4">
-              <a href="<?php echo $link; ?>">
-                  <div class="card card-posts">
-                      <?php the_post_thumbnail('medium', array('class' => 'card-img-top img-fluid')); ?>
-                      <div class="card-body">
-                          <h5 class="card-title card-text title-card-blog"><?php echo wp_trim_words( get_the_title(), 14, '...' ); ?></h5>
-                          <h6><?php the_category(); ?></h6>
-                          <p class="card-text"><?php echo wp_trim_words( get_the_content(), 19, ' [...] ' ); ?></p>
-                          <a href="<?php echo $link; ?>"><button class="btn botao-laranja">Acesse o Material</button></a>
-                      </div>
-                  </div>
-              </a>
+    <div class="blog-floater">
+      <div class="container">
+        <div class="row"> 
+          <div class="col-md-3">
+            <svg class="svg-inline--fa fa-home fa-w-18" aria-hidden="true" data-icon="home" data-prefix="fas" role="img" viewBox="0 0 576 512" xmlns="http://www.w3.org/2000/svg">
+            <path d="M488 312.7V456c0 13.3-10.7 24-24 24H348c-6.6 0-12-5.4-12-12V356c0-6.6-5.4-12-12-12h-72c-6.6 0-12 5.4-12 12v112c0 6.6-5.4 12-12 12H112c-13.3 0-24-10.7-24-24V312.7c0-3.6 1.6-7 4.4-9.3l188-154.8c4.4-3.6 10.8-3.6 15.3 0l188 154.8c2.7 2.3 4.3 5.7 4.3 9.3zm83.6-60.9L488 182.9V44.4c0-6.6-5.4-12-12-12h-56c-6.6 0-12 5.4-12 12V117l-89.5-73.7c-17.7-14.6-43.3-14.6-61 0L4.4 251.8c-5.1 4.2-5.8 11.8-1.6 16.9l25.5 31c4.2 5.1 11.8 5.8 16.9 1.6l235.2-193.7c4.4-3.6 10.8-3.6 15.3 0l235.2 193.7c5.1 4.2 12.7 3.5 16.9-1.6l25.5-31c4.2-5.2 3.4-12.7-1.7-16.9z"/>
+            </svg>
+            <?php wp_custom_breadcrumbs() ?>
           </div>
-          <?php endwhile;?>
+          <div class="col-md-6 formulario">
+            <?php $search = isset( $_GET['searchkey'] ) ? $_GET['searchkey'] : '';?>
+            <form ROLE="search" action="<?php echo($_SERVER['REQUEST_URI']); ?>" method="get">
+              <div>
+                <label class="screen-reader-text" for="s">Pesquisar por:</label>
+                <input type="text" value="<?php echo($search); ?>" name="searchkey" id="searchkey">
+                <input type="submit" id="searchsubmit" value="Pesquisar">
+              </div>
+            </form>    
+          </div>
+        </div>  
+      </div>       
+    </div>
+
+    <div class="container blog">
+      <div class="row">
+        <?php
+        $postsPerPage = get_option( 'posts_per_page' );
+        $paged = isset( $_GET['sheet'] )? $_GET['sheet'] : 1;
+        $args = array(
+          'posts_per_page' => $postsPerPage,
+          'paged' => $paged,
+          'post_type' => 'materiais',
+        );
+        $card = new WP_Query($args);
+        while($card->have_posts()) : $card->the_post();
+        $link = get_field('link'); ?>
+        <div class="col-md-4">
+          <a href="<?php echo $link; ?>">
+            <div class="card card-posts material">
+              <?php the_post_thumbnail('medium', array('class' => 'card-img-top img-fluid')); ?>
+              <div class="card-body">
+                <h5 class="card-title card-text title-card-blog"><?php echo wp_trim_words( get_the_title(), 14, '...' ); ?></h5>
+                <h6><?php the_category(); ?></h6>
+                <p class="card-text"><?php echo wp_trim_words( get_the_content(), 19, ' [...] ' ); ?></p>
+                <a href="<?php echo $link; ?>"><button class="btn botao-laranja">Acesse o Material</button></a>
+              </div>
+            </div>
+          </a>
         </div>
+        <?php endwhile;?>
+      </div>
     </div>
     <!-- paginação -->
     <div class="row container paginate-container">
