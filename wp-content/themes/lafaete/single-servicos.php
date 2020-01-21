280f9ba398c10158ddfe9fe712b7c2d6d0a23c1f
PedroM <pedro@dnadevendas.com.br>
2020-01-21 11:14:32 -0300

    remove class d-none

--- wp-content/themes/lafaete/single-servicos.php
+++ wp-content/themes/lafaete/single-servicos.php
@@ -11,7 +11,7 @@
     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
       <div class="carousel-inner carousel-flat-height">
         <div class="carousel-item active">
-          <div class="carousel-caption carousel-caption-flat-height d-none d-md-block">
+          <div class="carousel-caption carousel-caption-flat-height d-md-block">
             <h1><?php the_title(); ?></h1>
           </div>
             <?php the_post_thumbnail('full'); ?>
