1448867a7c980c8d32065d2454466af0abbe262c
PedroM <pedro@dnadevendas.com.br>
2020-02-14 13:06:09 -0300

    fix merge

--- wp-content/themes/lafaete/head.php
+++ wp-content/themes/lafaete/head.php
@@ -1,4 +1,17 @@
 <head>
+<!-- Required meta tags -->
+<script async="" src="https://www.google-analytics.com/analytics.js"></script>
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-34414227-1', 'auto');
+ga('send', 'pageview');
+</script>
+<!-- End Google Analytics -->
     <!-- Required meta tags -->
     <meta charset="UTF-8">
     <meta name="description" content="Lafaete Locação">
